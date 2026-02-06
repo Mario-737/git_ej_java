1c7f235bcedfe89c23fa89cf16ba4d035455a77d
Mario <mreald05@iesalbarregas.es>
2026-02-06 19:21:36 +0100

    Version 2

--- Ej2.java
+++ Ej2.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Ej2 {
     public static void main(String[] args) {
         int[] edades = new int[5];
+        int mayor = 0;
+        int menor = 9999999;
 
         Scanner teclado = new Scanner(System.in);
         
@@ -10,6 +12,12 @@ public class Ej2 {
             System.out.println("Introduce una edad:");
             int edad = teclado.nextInt();
             edades[i]=edad;
+            if (edad>mayor) {
+                mayor=edad;
+            }
+            if (edad<menor) {
+                menor=edad;
+            }
         }
         teclado.close();
         
@@ -17,6 +25,7 @@ public class Ej2 {
             System.out.print(i+", ");
         }
         System.out.println();
+        System.out.printf("La mayor edad es %d y la menor es %d\n",mayor,menor);
 
     }
 }
