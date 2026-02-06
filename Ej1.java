d247cb6b60d0dc5828a9923ca98b17bfba47c16d
Mario <mreald05@iesalbarregas.es>
2026-02-06 18:26:41 +0100

    Version 3

--- Ej1.java
+++ Ej1.java
@@ -4,6 +4,10 @@ public class Ej1 {
     public static void main(String[] args) {
         double[] numeros = new double[5];
         double suma = 0;
+        double mayor = 0;
+        double menor = 9999999;
+        int positivos = 0;
+        int negativos = 0;
 
         Scanner teclado = new Scanner(System.in);
         
@@ -12,6 +16,19 @@ public class Ej1 {
             int numero = teclado.nextInt();
             numeros[i]=numero;
             suma += numero;
+            if (numero>mayor) {
+                mayor=numero;
+            }
+            if (numero<menor) {
+                menor=numero;
+            }
+            if (numero>0) {
+                positivos++;
+            }
+            if (numero<0) {
+                negativos++;
+            }
+
         }
         teclado.close();
         
@@ -21,6 +38,8 @@ public class Ej1 {
         System.out.println();
         System.out.println("La suma es: "+suma);
         System.out.println("La media es: "+suma/5);
+        System.out.printf("El mayor es %.1f y el menor es %.1f\n",mayor,menor);
+        System.out.printf("Hay %d positivos y %d negativos\n",positivos,negativos);
 
 
     }
