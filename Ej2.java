c8ea16722e993202f6a63f57851588b8eba8de2f
Mario <mreald05@iesalbarregas.es>
2026-02-15 12:36:59 +0100

    Version 4

--- Ej2.java
+++ Ej2.java
@@ -7,6 +7,9 @@ public class Ej2 {
         int menor = 9999999;
         int mayorDeEdad = 0;
         int menorDeEdad = 0;
+        int joven = 0;
+        int adulto = 0;
+        int anciano = 0;
 
         Scanner teclado = new Scanner(System.in);
         
@@ -20,21 +23,34 @@ public class Ej2 {
             if (edad<menor) {
                 menor=edad;
             }
+
             if (edad>18) {
                 mayorDeEdad++;
             }
-            if (edad<14) {
+            if (edad<18) {
                 menorDeEdad++;
+            }
+
+            if (edad>=19 && edad<30) {
+                joven++;
+            } else if (edad>=30 && edad<50) {
+                adulto++;
+            } else if (edad>=50) {
+                anciano++;
             }
 
         }
         teclado.close();
         
-        for (double i : edades) {
+        for (int i : edades) {
             System.out.print(i+", ");
         }
         System.out.println();
         System.out.printf("La mayor edad es %d y la menor es %d\n",mayor,menor);
         System.out.printf("Hay %d mayores de edad y %d menores de edad\n",mayorDeEdad,menorDeEdad);
+        System.out.printf("0-17: %d\n", menorDeEdad);
+        System.out.printf("18-30: %d\n", joven);
+        System.out.printf("31-50: %d\n", adulto);
+        System.out.printf("+51: %d\n", anciano);
     }
 }
